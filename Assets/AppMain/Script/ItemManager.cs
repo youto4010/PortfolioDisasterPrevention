89654f853073062261df5738b6c3f505672a3f8b
youto4010 <83483571+youto4010@users.noreply.github.com>
2022-08-29 23:04:45 +0900

    item.cs itemManager.csを制作

--- Assets/AppMain/Script/ItemManager.cs
+++ Assets/AppMain/Script/ItemManager.cs
@@ -2,29 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemManager : MonoBehaviour
-{ 
-    static ItemManager instance;
-
-    public static ItemManager GetInstance();
-    [
-        return instance;
-    ]
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        instance = this;
-    }
-
-    public bool HasItem(string searchName)
-    {
-        return itemDataBase.GetItemLists().Exists(items => item.GetItemName() = searchName);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+[CreateAssetMenu(fileName = "ItemDataBase", menuName = "CreateItemDataBase")]
+public class ItemDataBase : ScriptableObject{
+    [SerializeField] private List<Item> itemLists = new List<Item>();
+    // アイテムリストを返す
+    public List<Item> GetItemLists(){
+        return itemLists;
     }
 }
