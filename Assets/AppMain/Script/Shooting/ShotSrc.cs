e57a20be6b79c1fa70c7cf80a3aec2fdcc0f0fd9
youto4010 <83483571+youto4010@users.noreply.github.com>
2022-10-23 20:48:53 +0900

    消火の流れを実装

--- Assets/AppMain/Script/Shooting/ShotSrc.cs
+++ Assets/AppMain/Script/Shooting/ShotSrc.cs
@@ -1,17 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class ShotSrc : MonoBehaviour
 {
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private Transform muzzle;
     [SerializeField] private float bulletPower = 500f;
+    [SerializeField] Slider FireBar= null;
+    private int WaterPoint = 1;
+    [System.Serializable]
+    public class Status
+    {
+        // 体力
+        public int Hp = 15;
+    }
+    // 基本ステータス
+    [SerializeField] Status DefaultStatus = new Status();
+    // 現在のステータス
+    public Status CurrentStatus = new Status();
     // Start is called before the first frame update
     void Start()
     {
-        // target = GameObject.Find("Target").gameObject;
-        // direction = target.transform.position;
+        // スライダーの初期設定
+        FireBar.maxValue = DefaultStatus.Hp;
+        FireBar.value = CurrentStatus.Hp;
     }
 
     // Update is called once per frame
@@ -23,8 +38,16 @@ public class ShotSrc : MonoBehaviour
             Debug.Log(muzzle.position);
             Debug.Log(muzzle.rotation.GetType());
             bulletInstance.GetComponent<Rigidbody>().AddForce(bulletInstance.transform.up*bulletPower);
+            CurrentStatus.Hp -= WaterPoint;
             // transform.position = Vector3.MoveTowards(transform.position,direction,step);
             Destroy(bulletInstance,5f);
         }
+
+        if(CurrentStatus.Hp == 0)
+        {
+            
+        }
     }
+
+
 }
